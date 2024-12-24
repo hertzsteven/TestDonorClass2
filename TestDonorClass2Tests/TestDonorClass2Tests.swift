ca5f88ae687f4d94e14ea9d1a69b50b5ee617d60
Steven Hertz <StevenH@appi4.com>
2024-12-24 09:22:23 -0500

    Initial Commit

---
+++ TestDonorClass2Tests/TestDonorClass2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TestDonorClass2Tests.swift
+//  TestDonorClass2Tests
+//
+//  Created by Steven Hertz on 12/24/24.
+//
+
+import Testing
+@testable import TestDonorClass2
+
+struct TestDonorClass2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
